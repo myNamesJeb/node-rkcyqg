9327c72ebb7fd64535735d294d6c348e42e4bf36
crazyHoe <103546253+crazyHoe@users.noreply.github.com>
2022-09-08 17:34:22 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 // run `node index.js` in the terminal
-const cols = require('./config.js')
-console.log(cols.fg.yellow, 'initiallizing...');
+const cols = require('./config.js');
+console.log('initiallizing...');
 
-const planet
+var sun = {
+    mass: 9.4,
+    name: 'sun',
+}
+
+class planet {
+  constructor(speed, mass, name) {
+    this.name = name;
+    this.speed = speed;
+    this.mass = mass
+    this.periapsis = Math.floor(speed/sun.mass*50-this.speed)
+    this.apoapsis = Math.floor(speed/sun.mass*50+this.speed)
+    this.distAwayFromSun = this.periapsis;
+  }
+  //calculating pull
+  init(thing){
+    while(thing != mes){
+      var mes = 0
+      for(i = this.periapsis; i != this.apoapsis; i++){
+        this.distAwayFromSun++
+        console.table( this.distAwayFromSun)
+      }
+      for(i = this.apoapsis; i != this.periapsis; i--){
+        this.distAwayFromSun--
+        console.table( this.distAwayFromSun)
+      }
+    }
+    mes++
+  }
+}
+
+var kerbin = new planet(100, 3, 'kerbin')
+kerbin.init(2)
