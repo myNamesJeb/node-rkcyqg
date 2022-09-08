4171c74afb8d8032fa73df32bd4b92a8510f5b35
crazyHoe <103546253+crazyHoe@users.noreply.github.com>
2022-09-08 17:56:25 -0400

    its done

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
 // run `node index.js` in the terminal
 const cols = require('./config.js');
 console.log('initiallizing...');
+function sleep(miliseconds) {
+  var currentTime = new Date().getTime();
 
-var sun = {
-    mass: 9.4,
-    name: 'sun',
+  while (currentTime + miliseconds >= new Date().getTime()) {}
 }
+var sun = {
+  mass: 9.4,
+  name: 'sun',
+};
 
 class planet {
   constructor(speed, mass, name) {
     this.name = name;
     this.speed = speed;
-    this.mass = mass
-    this.periapsis = Math.floor(speed/sun.mass*50-this.speed)
-    this.apoapsis = Math.floor(speed/sun.mass*50+this.speed)
+    this.mass = mass;
+    this.periapsis = Math.floor((speed / sun.mass) * 50 - this.speed);
+    this.apoapsis = Math.floor((speed / sun.mass) * 50 + this.speed);
     this.distAwayFromSun = this.periapsis;
   }
   //calculating pull
-  init(thing){
-    while(thing != mes){
-      var mes = 0
-      for(i = this.periapsis; i != this.apoapsis; i++){
-        this.distAwayFromSun++
-        console.table( this.distAwayFromSun)
-        console.clear()
+  init(thing) {
+    while (thing != mes) {
+      var mes = 0;
+      for (i = this.periapsis; i != this.apoapsis; i++) {
+        if (i % 2 === 0) {
+          this.speed--;
+        }
+        this.distAwayFromSun++;
+        console.table([{ height: this.distAwayFromSun, speed: this.speed }]);
+        sleep(this.speed);
+        console.clear();
       }
-      for(i = this.apoapsis; i != this.periapsis; i--){
-        this.distAwayFromSun--
-        console.table( this.distAwayFromSun)
-        console.clear()
+      for (i = this.apoapsis; i != this.periapsis; i--) {
+        if (i % 2 === 0) {
+          this.speed++;
+        }
+        this.distAwayFromSun--;
+        console.table({ height: this.distAwayFromSun, speed: this.speed });
+        sleep(this.speed);
+        console.clear();
       }
     }
-    mes++
+    mes++;
   }
 }
 
-var kerbin = new planet(100, 3, 'kerbin')
-kerbin.init(2)
+var kerbin = new planet(100, 3, 'kerbin');
+kerbin.init(2);
